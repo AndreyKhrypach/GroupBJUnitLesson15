a0c31f7dd8e6c79f439b06d6bc1bfa77630f51c9
AndreyKhrypach <pacik78@gmail.com>
2023-09-12 20:17:30 +0300

    first commit

---
+++ src/main/java/Lesson15Program.java
@@ -0,0 +1,5 @@
+public class Lesson15Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/HomeAnimalTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeAnimalTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAnimalType() {
+    }
+
+    @Test
+    void setAnimalType() {
+    }
+
+    @Test
+    void getBark() {
+    }
+
+    @Test
+    void setBark() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void isSex() {
+    }
+
+    @Test
+    void setSex() {
+    }
+}
